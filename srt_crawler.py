fc44b74ca37e0448119c7a9bfcd6a925cff307f0
Jiyeon <ji89yeon@gmail.com>
2018-12-10 18:00:49 +0900

    Add Function for changing value
    There is a problem with change secondary element, arvRsStnCdNm.

--- srt_crawler.py
+++ srt_crawler.py
@@ -1,8 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.common.exceptions import NoSuchElementException
-
-#driver = webdriver.PhantomJS('./phantomjs')
+from selenium.webdriver.common.keys import Keys
 
 ch_options = webdriver.ChromeOptions()
 ch_options.add_argument('headless')
@@ -14,11 +13,23 @@ driver.implicitly_wait(3)
 
 driver.get("https://etk.srail.co.kr/hpg/hra/01/selectScheduleList.do?pageId=TK0101010000")
 
-try:
-	elem = driver.find_element_by_name('dptRsStnCdNm')
-	print (elem.get_attribute('value'))
-except NoSuchElementException:
-	print("Cannot find")
+def changeAttributeValue (attr_name, attr_value):
+	try:
+		elem = driver.find_element_by_name(attr_name)
+		prvValue = elem.get_attribute('value')
+		elem.clear()
+		elem.send_keys(attr_value)
+		elem.send_keys(Keys.RETURN)
+		print ("Changed " + prvValue + " to " + elem.get_attribute('value'))
+		driver.implicitly_wait(1)
+	except NoSuchElementException:
+		print("Cannot find element " + attr_name)
+		
+		
+changeAttributeValue('dptRsStnCdNm', "신경주")
+changeAttributeValue('arvRsStnCdNm', "동탄")
+changeAttributeValue('dptDt', "20181216")
+changeAttributeValue('dptTm', "120000")
 
 driver.save_screenshot('./test.png')
 print("Quit")
